c8f8d9e68556dd8d8e0d26c30c881c37118415a8
lariskau <bougaut.lea@gmail.com>
2019-07-08 17:32:38 +0200

    pad implemented

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+require('./signature_pad');
 
 window.Vue = require('vue');
 
--- resources/views/signature.blade.php
+++ resources/views/signature.blade.php
@@ -9,6 +9,9 @@
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"/>
     </head>
     <body>
+
+<!-- Basic form -->
+
       <h1>Signature</h1>
       <div class="container">
          <div class="alert alert-success" style="display:none"></div>
@@ -19,11 +22,30 @@
             </div>
             <button class="btn btn-primary" id="ajaxSubmit">Submit</button>
           </form>
+
+<!-- Signature pad -->
+          <div class="wrapper">
+            <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
+          </div>
+
+          <button id="save-png">Save as PNG</button>
+          <button id="clear">Clear</button>
+
+
+<!-- CDN -->
+
       </div>
       <script src="http://code.jquery.com/jquery-3.3.1.min.js"
                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
                crossorigin="anonymous">
       </script>
+      <script src="{{ url('js/app.js') }} " charset="utf-8"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+
+
+<!-- AJAX to save basic form -->
+
       <script>
          jQuery(document).ready(function(){
             jQuery('#ajaxSubmit').click(function(e){
@@ -46,5 +68,41 @@
                });
             });
       </script>
+
+<!-- Signature pad js -->
+
+      <script type="text/javascript">
+      var canvas = document.getElementById('signature-pad');
+
+      function resizeCanvas() {
+          var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+          canvas.width = canvas.offsetWidth * ratio;
+          canvas.height = canvas.offsetHeight * ratio;
+          canvas.getContext("2d").scale(ratio, ratio);
+      }
+
+      window.onresize = resizeCanvas;
+      resizeCanvas();
+
+      var signaturePad = new SignaturePad(canvas, {
+        backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
+      });
+
+      document.getElementById('save-png').addEventListener('click', function () {
+        if (signaturePad.isEmpty()) {
+          return alert("Please provide a signature first.");
+        }
+
+          var data = signaturePad.toDataURL('image/png');
+            console.log(data);
+            window.open(data);
+          });
+
+      document.getElementById('clear').addEventListener('click', function () {
+        signaturePad.clear();
+      });
+
+      </script>
+
     </body>
 </html>
