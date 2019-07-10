56e284e7356c99bab8f85d8e707144ebf01391ef
lariskau <bougaut.lea@gmail.com>
2019-07-10 12:40:59 +0200

    alert to confirm that the data is stored added

--- app/Http/Controllers/SignatureController.php
+++ app/Http/Controllers/SignatureController.php
@@ -11,13 +11,19 @@ class SignatureController extends Controller
 {
         public function store(Request $request)
         {
-             $filename = date('mdYHis') . "signature.png";
+             // We create a variable to define the name of the file
+             // Here it's the date and the extension signature.png
+             $filename = date('mdYHis') . "-signature.png";
+             // We store the signature file name in DB
              $signature = new Signature();
              $signature->file = $filename;
              $signature->save();
+             // We decode the image and store it in public folder
              $data_uri = $request->signature;
              $encoded_image = explode(",", $data_uri)[1];
              $decoded_image = base64_decode($encoded_image);
              file_put_contents($filename, $decoded_image);
+             // Text of the alter to confirm that the data is posted
+             return response()->json(['success'=>'Data is successfully added']);
         }
 }
--- resources/views/signature.blade.php
+++ resources/views/signature.blade.php
@@ -10,38 +10,44 @@
     </head>
     <body>
 
-      <!-- Signature pad -->
+<!-- This simple signature pad form register the signatures with AJAX  -->
+<!-- The signatures are stored in Public folder and in DB (here signature -> file) -->
+
+
+<!-- Signature pad form made with Boostrap components -->
+
       <br>
-      <div class="container">
-        <div class="jumbotron">
+        <div class="container">
+          <div class="jumbotron">
 
-        <h1 class="display-6">Signature pad library</h1>
-        <p class="lead">Simple form made with Laravel framework and Ajax POST method</p>
+            <h1 class="display-6">Signature pad library</h1>
+            <p class="lead">Simple form made with Laravel framework and Ajax POST method</p>
 
-          <div class="wrapper">
-            <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
-          </div>
-          <br>
-          <button class="btn btn-primary" id="save">Save</button>
-          <button class="btn btn-secondary" id="clear">Clear</button>
+            <div class="alert alert-success" style="display:none"></div>
+
+              <div class="wrapper">
+                <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
+              </div>
+
+              <br>
+              <button class="btn btn-primary" id="save">Save</button>
+              <button class="btn btn-secondary" id="clear">Clear</button>
+
+            </div>
         </div>
 
-        </div>
 
-
-<!-- CDN -->
-
-      </div>
+<!-- Link to js file in folder app and CDN used for the signature pad :  Jquery, ajax, signature pad -->
+      <script src="{{ url('js/app.js') }} " charset="utf-8"></script>
       <script src="http://code.jquery.com/jquery-3.3.1.min.js"
                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
                crossorigin="anonymous">
       </script>
-      <script src="{{ url('js/app.js') }} " charset="utf-8"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
 
 
-<!-- AJAX to save basic form -->
+<!-- AJAX to save signature pad content -->
 
       <script>
          $(function(){
@@ -81,9 +87,6 @@
             });
       </script>
 
-<!-- Signature pad js -->
-
-
-
     </body>
+
 </html>
